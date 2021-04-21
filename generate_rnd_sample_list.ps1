bfbf1bf6895aa516f777b0caca1dc9b7006ea3d0
w3ich3rt <68917715+w3ich3rt@users.noreply.github.com>
2021-04-21 21:20:53 +0200

    some minor changes
    added new parameter and changed the variables.

--- generate_rnd_sample_list.ps1
+++ generate_rnd_sample_list.ps1
@@ -1,21 +1,24 @@
 <#
 .SYNOPSIS
-	Will generate a random list from a given List.
+	Will generate a random sample list - for audits or so - from a given list.
 	
 .DESCRIPTION
-	In case you need to generate a sample for your internal control system, you could use this script.
-	The script then will generate a new Excelfile with the samples for you. Completly random.
+	In case you need to generate a sample for your internal control system or audit, you could use this script.
+	The script then will generate a new Excelfile with the samples for you or the auditor. Completly random.
 
 .Parameter SourceFile 
 	Defines the sourcefile from which the sample is generated.
 .Parameter OutFile 
 	Defines the outputfile for the sample.
+.Parameter SampleSize
+	Defines the amount of entries in your sample-list.
 	
 .INPUTS
-	Needs to be written.
+	You have to define an excel-list with parameter SourceFile.
+	In general it will get 40 entries from you List - you can change that
 
 .OUTPUTS
-	Needs to be written.
+	You will get an excel-list with the same data as the inputfile but only the amount of columns you chose.
 
 .EXAMPLE
 	Needs to be written.
@@ -43,29 +46,24 @@ param(
 	[string]$SourceFile,
 	[Parameter(Mandatory=$false,ParameterSetName="OutFile",HelpMessage="File where the output will be written.")]
 	[Alias('output')]
-	[string]$OutFile="iks-sample.xlsx"
+	[string]$OutFile="iks-sample.xlsx",
+	[Parameter(Mandatory=$false,ParameterSetName="AmountSample",HelpMessage="Amount of sample in you generated list")]
+	[Alias('amountsample')]
+	[string]$amount_sample="40"
 )
 
-$excelfile = Import-Excel -Path Tickets.xlsx ;
-$amount_ticket = 40;
+$SampleObject = Import-Excel -Path $SourceFile
 
 ## install & import modules or namespaces
-Set-ExecutionPolicy Bypass -Scope Process
 Install-Module -Name ImportExcel -scope CurrentUser
 Import-Module -Name ImportExcel
 
-## define runtime
-$ErrorActionPreference = [ActionPreference]::Stop
-Set-StrictMode -Version 'Latest'
-
 ## main function
 
-$randomlist = for ($i=1; $i -le $amount_ticket; $i++) { 
-	$excelfile[(Get-Random -Maximum ($excelfile.count))]; 
+$randomlist = for ($i=1; $i -le $amount_sample; $i++) { 
+	$SampleObject[(Get-Random -Maximum ($SampleObject.count))]; 
 }
 
 $randomlist | Export-Excel
-
-## cleanup (if necessary)
 
 ## digital signature
