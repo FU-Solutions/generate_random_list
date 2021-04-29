b7bea45a5e4f55d160b3cecded285180cdd4ce1a
Ulli Weichert <ullrich@weichert.it>
2021-04-29 22:14:32 +0200

    Added requested feature from featurerequest by thm (github). Now there is power!

--- generate_rnd_sample_list.ps1
+++ generate_rnd_sample_list.ps1
@@ -15,6 +15,8 @@
 .Parameter SampleSize
 	Defines the amount of entries in your sample-list.
 	Default are 40 samples.
+.Parameter poWer
+    This switch allows you to use more power for your request! Just use switch -w and lay back.
 	
 .INPUTS
 	You have to define an excel-list with parameter SourceFile.
@@ -30,7 +32,8 @@
 	File Name		: generate_random_list.ps1
 	Author			: Ulli Weichert
 	Contact			: ulli@weichert.it - Weichert.IT
-	Version			: 0.1
+	Version			: 0.2
+    Release         : Beavers are bored.
 	
 	Introduction:
 		In case you need to generate a sample for your internal control system, you could use this script.
@@ -56,19 +59,46 @@ param(
 	[string]$OutFile="iks-sample.xlsx",
 	[Parameter(Mandatory=$false,HelpMessage="Amount of sample in you generated list")]
 	[Alias('samples')]
-	[Int]$SampleSize="40"
+	[Int]$SampleSize="40",
+    [Parameter(Mandatory=$false,HelpMessage="Will provide more power!")]
+    [Alias('w')]
+    [switch]$poWer=$true
 )
-
-$SampleObject = Import-Excel -Path $SourceFile
 
 ## install & import modules or namespaces
 Install-Module -Name ImportExcel -scope CurrentUser
 Import-Module -Name ImportExcel
 
+# function
+function use-power () {
+    if ( $poWer ) {
+        $wumms = @"
+__/\\\______________/\\\__/\\\________/\\\__/\\\________/\\\__/\\\\____________/\\\\__/\\\\____________/\\\\_____/\\\\\\\\\\\_______/\\\____        
+ _\/\\\_____________\/\\\_\/\\\_______\/\\\_\/\\\_______\/\\\_\/\\\\\\________/\\\\\\_\/\\\\\\________/\\\\\\___/\\\/////////\\\___/\\\\\\\__       
+  _\/\\\_____________\/\\\_\/\\\_______\/\\\_\/\\\_______\/\\\_\/\\\//\\\____/\\\//\\\_\/\\\//\\\____/\\\//\\\__\//\\\______\///___/\\\\\\\\\_      
+   _\//\\\____/\\\____/\\\__\/\\\_______\/\\\_\/\\\_______\/\\\_\/\\\\///\\\/\\\/_\/\\\_\/\\\\///\\\/\\\/_\/\\\___\////\\\_________\//\\\\\\\__     
+    __\//\\\__/\\\\\__/\\\___\/\\\_______\/\\\_\/\\\_______\/\\\_\/\\\__\///\\\/___\/\\\_\/\\\__\///\\\/___\/\\\______\////\\\_______\//\\\\\___    
+     ___\//\\\/\\\/\\\/\\\____\/\\\_______\/\\\_\/\\\_______\/\\\_\/\\\____\///_____\/\\\_\/\\\____\///_____\/\\\_________\////\\\_____\//\\\____   
+      ____\//\\\\\\//\\\\\_____\//\\\______/\\\__\//\\\______/\\\__\/\\\_____________\/\\\_\/\\\_____________\/\\\__/\\\______\//\\\_____\///_____  
+       _____\//\\\__\//\\\_______\///\\\\\\\\\/____\///\\\\\\\\\/___\/\\\_____________\/\\\_\/\\\_____________\/\\\_\///\\\\\\\\\\\/_______/\\\____ 
+        ______\///____\///__________\/////////________\/////////_____\///______________\///__\///______________\///____\///////////________\///_____
+"@
+        
+        Write-Host -ForegroundColor Red $wumms
+
+    } else {}
+}
+
 ## main function
 
+# do we need more power?
+use-power
+
+# create random list
+$SampleObject = Import-Excel -Path $SourceFile
 $randomlist = for ($i=1; $i -le $SampleSize; $i++) { 
 	$SampleObject[(Get-Random -Maximum ($SampleObject.count))]; 
 }
 
+# write list
 $randomlist | Export-Excel $OutFile -WorksheetName Samplelist -AutoSize -AutoFilter -TableName "Samples"
